3e1a31d8cb023616993110da91ea00d97bdea9cd
Cristian-M-B <crisbaronetto@hotmail.com>
2021-09-08 17:13:26 -0300

    alta de usuario

--- src/Components/addUser/AddUser.jsx
+++ src/Components/addUser/AddUser.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/styles';
-import { Grid, TextField, Button, Snackbar } from '@material-ui/core';
-import { Check, Close } from '@material-ui/icons';
+import { FormHelperText, FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton, Typography, Container, Paper, Grid, TextField, Button, Snackbar } from '@material-ui/core';
+import { Visibility, VisibilityOff, Check, Close } from '@material-ui/icons';
 import { Alert } from '@material-ui/lab';
 
 const useStyles = makeStyles((theme) => ({
     gridContainer: {
-        marginTop:'5vh',
+        marginTop:'1vh',
+        // height:'100%',
+        // alignItems:"center",
     },
     textField: {
         marginBottom: '2vh',
-        width:'24vw',
+        minWidth:'24vw',
+        // justifyContent:"center",
+    },
+    formHelper:{
+        marginTop: '-1.5vh',
     },
     buttom: {
         backgroundColor: '#8e0000',
         color: '#ffffff',
-        marginLeft:'1vh',
+        margin:'1vh',
     },
 }))
 
@@ -23,14 +30,17 @@ export default function AddUser(){
 
     const classes = useStyles();
 
-    const [open, setOpen] = useState(false);
+    const history = useHistory();
+    const [openSuccess, setOpenSuccess] = useState(false);
+    const [openError, setOpenError] = useState(false);
     const [error, setError] = useState({});
     const [input, setInput] = useState({
-        firstName:'',
-        lastName:'',
+        name:'',
+        surname:'',
         document:'',
         email:'',
-        password:''
+        password:'',
+        showPassword: false,
     })
 
     const handleClose = (event, reason) => {
@@ -38,32 +48,33 @@ export default function AddUser(){
             return;
         }
     
-        setOpen(false);
+        setOpenSuccess(false);
+        setOpenError(false);
     };
 
     function validate(input){
         let error = {};
 
-        if(!input.firstName){
-            error.firstName = 'Debe ingresar su nombre'
-        } else if(!/^[a-zA-Z,.'-]+$/u.test(input.firstName)){
-            error.firstName = 'El nombre no es válido'
-        } else if(input.firstName.length < 3){
-            error.firstName = 'El nombre debe tener un minimo de 3 letras'
+        if(!input.name){
+            error.name = 'Debe ingresar su nombre'
+        } else if(!/^[a-zA-Z ,.'-]+$/u.test(input.name)){
+            error.name = 'El nombre no es válido'
+        } else if(input.name.length < 3){
+            error.name = 'El nombre debe tener un minimo de 3 letras'
         }
 
-        if(!input.lastName){
-            error.lastName = 'Debe ingresar su apellido'
-        } else if(!/^[a-zA-Z,.'-]+$/u.test(input.lastName)){
-            error.lastName = 'El apellido no es válido'
-        } else if(input.lastName.length < 3){
-            error.lastName = 'El apellido debe tener un minimo de 3 letras'
+        if(!input.surname){
+            error.surname = 'Debe ingresar su apellido'
+        } else if(!/^[a-zA-Z ,.'-]+$/u.test(input.surname)){
+            error.surname = 'El apellido no es válido'
+        } else if(input.surname.length < 3){
+            error.surname = 'El apellido debe tener un minimo de 3 letras'
         }
 
         if(!input.document){
             error.document = 'Debe ingresar su número de documento'
         } else if(!/^[0-9]+$/u.test(input.document)){
-            error.document = 'Solo ingrese los numeros'
+            error.document = 'Solo ingrese números'
         } else if(input.document.length !== 8){
             error.document = 'El documento debe tener 8 números'
         } // else if(){
@@ -72,18 +83,15 @@ export default function AddUser(){
 
         if(!input.email){
             error.email = 'Debe ingresar su email'
-        } //else if(!/^[a-zA-Z,.'-]+$/u.test(input.email)){
-        //     error.email = 'El apellido no es válido'
-        // } else if(input.email.length < 3){
-        //     error.email = 'El apellido debe tener un minimo de 3 letras'
-        // } // else if(){
+        } else if(!/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(input.email)){
+            error.email = 'El email no es válido'
+        } // else if(){
             // error.email = 'Ya hay un usuario registrado con ese email'
         //}
         
-
         if(!input.password){
             error.password = 'Debe ingresar su contraseña'
-        } else if(!/^[a-zA-Z0-9()-_.]+$/u.test(input.password)){
+        } else if(!/^[a-zA-Z0-9.-_()]+$/u.test(input.password)){
             error.password = 'Solo ingrese caracteres válidos'
         } else if(input.password.length < 8 || input.password.length > 16){
             error.password = 'La contraseña debe tener entre 8 y 16 caracteres'
@@ -105,80 +113,129 @@ export default function AddUser(){
 
     function handleSubmit(e){
         e.preventDefault();
-        <Snackbar open={true} autoHideDuration={3000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" variant="filled">
-            Cuenta creada exitosamente!
-        </Alert>
-        </Snackbar>
+        setInput({
+            name:'',
+            surname:'',
+            document:'',
+            email:'',
+            password:''
+        });
+        setOpenSuccess(true);
+        // setOpenError(true);
     }
 
-    return <form onSubmit={handleSubmit}>
-    <Grid container direction="column" justifyContent="center" alignItems="center" className={classes.gridContainer}>
-        {/* <form submit={handleSubmit}> */}
-        <TextField
-            className={classes.textField}
-            required
-            name="firstName"
-            value={input.firstName}
-            label="Nombre/s"
-            variant="outlined"
-            onChange={handleInput}
-            helperText={error.firstName}
-        />
-        <TextField
-            className={classes.textField}
-            required
-            name="lastName"
-            value={input.lastName}
-            label="Apellido/s"
-            variant="outlined"
-            onChange={handleInput}
-            helperText={error.lastName}
-        />
-        <TextField
-            className={classes.textField}
-            required
-            name="document"
-            value={input.document}
-            label="N° de documento"
-            variant="outlined"
-            onChange={handleInput}
-            helperText={error.document}
-        />
-        <TextField
-            className={classes.textField}
-            required
-            name="email"
-            value={input.email}
-            label="E-mail"
-            variant="outlined"
-            onChange={handleInput}
-            helperText={error.email}
-        />
-        <TextField
-            className={classes.textField}
-            required
-            name="password"
-            value={input.password}
-            label="Contraseña"
-            type="password"
-            variant="outlined"
-            onChange={handleInput}
-            helperText={error.password}
-        />
-        <Grid container direction="row" justifyContent="center" alignItems="center">
-            <Button type="submit" variant="contained" color="primary" endIcon={<Check />}>
-                Crear cuenta
-            </Button>
-                
-            {/* <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="success" variant="filled">
-                    Cuenta creada exitosamente!
-                </Alert>
-            </Snackbar> */}
-            <Button className={classes.buttom} variant="contained" endIcon={<Close />}>Cancelar</Button>
-        </Grid>
-        {/* </form> */}
+    function handleClick(e){
+        setInput({
+            name:'',
+            surname:'',
+            document:'',
+            email:'',
+            password:''
+        });
+        history.push('/products');
+    }
+
+    function handleVisibilityPassword(){
+        setInput({
+            ...input,
+            showPassword: !input.showPassword
+        })
+    }
+
+    return (
+    <Grid container component="main" className={classes.gridContainer}>
+        <Container component={Paper} elevation={24} style={{ padding: '2vh', maxWidth: '26vw' }}>
+            <Typography align='center' variant='h6' color='primary' style={{ marginBottom: '1vh' }}>Registrate</Typography>
+            <form onSubmit={handleSubmit}>
+
+                <TextField
+                    className={classes.textField}
+                    required
+                    name="name"
+                    value={input.name}
+                    label="Nombre/s"
+                    variant="outlined"
+                    onChange={handleInput}
+                    helperText={error.name}
+                />
+
+                <TextField
+                    className={classes.textField}
+                    required
+                    name="surname"
+                    value={input.surname}
+                    label="Apellido/s"
+                    variant="outlined"
+                    onChange={handleInput}
+                    helperText={error.surname}
+                />
+
+                <TextField
+                    className={classes.textField}
+                    required
+                    name="document"
+                    value={input.document}
+                    label="N° de documento"
+                    variant="outlined"
+                    onChange={handleInput}
+                    helperText={error.document}
+                />
+
+                <TextField
+                    className={classes.textField}
+                    required
+                    name="email"
+                    value={input.email}
+                    label="E-mail"
+                    variant="outlined"
+                    onChange={handleInput}
+                    helperText={error.email}
+                />
+
+                <FormControl variant="outlined">
+                    <InputLabel htmlFor="contraseña">Contraseña *</InputLabel>
+                    <OutlinedInput
+                        id="contraseña"
+                        className={classes.textField}
+                        required
+                        name="password"
+                        value={input.password}
+                        label="contraseña *"
+                        type={input.showPassword ? "text" : "password"}
+                        onChange={handleInput}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton edge="end" onClick={handleVisibilityPassword}>
+                                    {input.showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                    <FormHelperText id="contraseña" className={classes.formHelper}>{error.password}</FormHelperText>
+                </FormControl>
+
+                <Grid container direction="row" justifyContent="center" alignItems="center">
+                    {!error.name && !error.surname && !error.document && !error.email && !error.password &&
+                        <Button type="submit" variant="contained" color="primary" endIcon={<Check />}>
+                            Crear cuenta
+                        </Button>
+                    }
+
+                    <Snackbar open={openSuccess} autoHideDuration={5000} onClose={handleClose}>
+                        <Alert onClose={handleClose} severity="success" variant="filled">
+                            Cuenta creada exitosamente!
+                        </Alert>
+                    </Snackbar>
+                    <Snackbar open={openError} autoHideDuration={5000} onClose={handleClose}>
+                        <Alert onClose={handleClose} severity="error" variant="filled">
+                            Hubo un error al crear la cuenta!
+                        </Alert>
+                    </Snackbar>
+
+                    <Button className={classes.buttom} variant="contained" endIcon={<Close />} onClick={handleClick}>Cancelar</Button>
+                </Grid>
+            </form>
+        </Container>
     </Grid>
-    </form>
+    )
 }
