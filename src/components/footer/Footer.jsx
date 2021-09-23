c1d9de5553a3e4f9af89c3b3a8f7f5c59903e2e3
gggerman <21german.garcia@gmail.com>
2021-09-23 15:18:13 -0300

    footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import {BottomNavigation, BottomNavigationAction, CssBaseline, Typography} from '@material-ui/core';
+import {BottomNavigation, BottomNavigationAction, CssBaseline, Typography, Container, Grid, Box, Link} from '@material-ui/core';
+import logo from '../nav/logo.jpg';
 
 const useStyles = makeStyles( (theme) => ({
     footer:{
@@ -15,18 +16,83 @@ const useStyles = makeStyles( (theme) => ({
 
 export default function Footer(){
     const classes = useStyles()
-    return (
-        //Para que el footer no tenga margenes blancos
-        //en los costados y abajo y ocupe todo el ancho
-        <CssBaseline>
-        <BottomNavigation className ={classes.footer}>
-            
-            <Typography> Sobre Nosotros </Typography>
-            <Typography> Development Team </Typography>
-            <Typography> Contacto </Typography>
-            <Typography> Ayuda </Typography>
-
-        </BottomNavigation>
-        </CssBaseline>
-    )
+    return <footer>
+      <Box
+      className={classes.footer}
+      px={{ xs: 3, sm: 10}}
+      py={{xs: 5, sm:10}}
+      >
+        <Container maxWidth="lg">
+          <Grid container spacing={5}>
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Acerca de</Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  ¿Cómo llego?
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Métodos de pago
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Métodos de envío
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Cambios y devoluciones
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Sobre nosotros
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Ayuda
+                </Link>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Contacto</Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  WhatsApp
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Email
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Dirección
+                </Link>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Redes Sociales</Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Facebook
+                </Link>
+              </Box>
+              <Box>
+                <Link href="/" color="inherit">
+                  Instagram
+                </Link>
+              </Box>
+            </Grid>
+          </Grid>
+          <Box textAlign="center" pt={{xs: 5, sm: 10}} pb={{xs: 5, sm:0}}>
+            Musical E-Commerce &reg; {new Date().getFullYear()}
+          </Box>
+        </Container>
+      </Box>
+    </footer>
 }
